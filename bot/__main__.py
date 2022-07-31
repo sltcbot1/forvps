4d7dc124f4ce2239962f6f9c027fd77700ad3889
chawije224 <106033248+chawije224@users.noreply.github.com>
2022-07-31 22:12:13 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -70,7 +70,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Join our our channel for any updates!!', context.bot, update.message, reply_markup)
+        sendMarkup('Join our channel for any updates!!', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
