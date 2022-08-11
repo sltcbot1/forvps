a907eaaae8c095d52f1f41954fb22b23abcfe287
sltcbot1 <107065974+sltcbot1@users.noreply.github.com>
2022-08-12 00:09:02 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -332,7 +332,7 @@ def _mirror(bot, message, isZip=False, extract=False, isQbit=False, isLeech=Fals
     else:
         tag = message.from_user.mention_html(message.from_user.first_name)
     
-    if link is not None:
+    if len(link) > 2:
        try:
            tag1 = message.from_user.mention_html(message.from_user.first_name)
            msg = f"<b>User <i>{tag1}</i> sent:</b>\n<code>{link}</code>"
