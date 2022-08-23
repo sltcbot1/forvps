aa28afa3e2c6c65644a1027d6ced24c00478637f
sltcbot1 <107065974+sltcbot1@users.noreply.github.com>
2022-08-23 15:37:28 +0530

    Update aria2_download.py

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -49,7 +49,7 @@ def __onDownloadStarted(api, gid):
                         msg += f'\nYour File/Folder size is {get_readable_file_size(size)}'
                         dl.getListener().onDownloadError(msg)
                         return api.remove([download], force=True, files=True)
-                if ZIP_UNZIP_LIMIT is not None and arch:
+                if ZIP_UNZIP_LIMIT is not None and (dl.getListener().isZip or dl.getListener.extract):
                     mssg = f'Zip/Unzip limit is {ZIP_UNZIP_LIMIT}GB'
                     limit = ZIP_UNZIP_LIMIT
                 if LEECH_LIMIT is not None and dl.getListener().isLeech:
