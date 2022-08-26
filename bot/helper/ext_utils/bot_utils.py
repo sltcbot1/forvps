2e191dc2f4dbd8f22596933415d588f20604318f
sltcbot1 <107065974+sltcbot1@users.noreply.github.com>
2022-08-26 19:19:16 +0530

    Add files via upload

---
+++ bot/helper/ext_utils/bot_utils.py
@@ -0,0 +1,307 @@
+from re import findall as re_findall, match as re_match
+from threading import Thread, Event
+from time import time
+from math import ceil
+from html import escape
+from psutil import virtual_memory, cpu_percent, disk_usage
+from requests import head as rhead
+from urllib.request import urlopen
+
+from bot import download_dict, download_dict_lock, STATUS_LIMIT, botStartTime, DOWNLOAD_DIR, WEB_PINCODE, BASE_URL
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.telegram_helper.button_build import ButtonMaker
+
+MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
+
+URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
+
+COUNT = 0
+PAGE_NO = 1
+
+
+class MirrorStatus:
+    STATUS_UPLOADING = "Uploading...📤"
+    STATUS_DOWNLOADING = "Downloading...📥"
+    STATUS_CLONING = "Cloning...♻️"
+    STATUS_WAITING = "Queued...💤"
+    STATUS_FAILED = "Failed 🚫. Cleaning Download..."
+    STATUS_PAUSED = "Paused...⛔️"
+    STATUS_ARCHIVING = "Archiving...🔐"
+    STATUS_EXTRACTING = "Extracting...📂"
+    STATUS_SPLITTING = "Splitting...✂️"
+    STATUS_CHECKING = "CheckingUp...📝"
+    STATUS_SEEDING = "Seeding...🌧"
+
+SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+
+class setInterval:
+    def __init__(self, interval, action):
+        self.interval = interval
+        self.action = action
+        self.stopEvent = Event()
+        thread = Thread(target=self.__setInterval)
+        thread.start()
+
+    def __setInterval(self):
+        nextTime = time() + self.interval
+        while not self.stopEvent.wait(nextTime - time()):
+            nextTime += self.interval
+            self.action()
+
+    def cancel(self):
+        self.stopEvent.set()
+
+def get_readable_file_size(size_in_bytes) -> str:
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+    except IndexError:
+        return 'File too large'
+
+def getDownloadByGid(gid):
+    with download_dict_lock:
+        for dl in list(download_dict.values()):
+            status = dl.status()
+            if (
+                status
+                not in [
+                    MirrorStatus.STATUS_ARCHIVING,
+                    MirrorStatus.STATUS_EXTRACTING,
+                    MirrorStatus.STATUS_SPLITTING,
+                ]
+                and dl.gid() == gid
+            ):
+                return dl
+    return None
+
+def getAllDownload(req_status: str):
+    with download_dict_lock:
+        for dl in list(download_dict.values()):
+            status = dl.status()
+            if status not in [MirrorStatus.STATUS_ARCHIVING, MirrorStatus.STATUS_EXTRACTING, MirrorStatus.STATUS_SPLITTING] and dl:
+                if req_status == 'down' and (status not in [MirrorStatus.STATUS_SEEDING,
+                                                            MirrorStatus.STATUS_UPLOADING,
+                                                            MirrorStatus.STATUS_CLONING]):
+                    return dl
+                elif req_status == 'up' and status == MirrorStatus.STATUS_UPLOADING:
+                    return dl
+                elif req_status == 'clone' and status == MirrorStatus.STATUS_CLONING:
+                    return dl
+                elif req_status == 'seed' and status == MirrorStatus.STATUS_SEEDING:
+                    return dl
+                elif req_status == 'all':
+                    return dl
+    return None
+
+def bt_selection_buttons(id_: str):
+    if len(id_) > 20:
+        gid = id_[:12]
+    else:
+        gid = id_
+
+    pincode = ""
+    for n in id_:
+        if n.isdigit():
+            pincode += str(n)
+        if len(pincode) == 4:
+            break
+
+    buttons = ButtonMaker()
+    if WEB_PINCODE:
+        buttons.buildbutton("Select Files", f"{BASE_URL}/app/files/{id_}")
+        buttons.sbutton("Pincode", f"btsel pin {gid} {pincode}")
+    else:
+        buttons.buildbutton("Select Files", f"{BASE_URL}/app/files/{id_}?pin_code={pincode}")
+    buttons.sbutton("Done Selecting", f"btsel done {gid} {id_}")
+    return buttons.build_menu(2)
+
+
+def get_progress_bar_string(status):
+    completed = status.processed_bytes() / 8
+    total = status.size_raw() / 8
+    p = 0 if total == 0 else round(completed * 100 / total)
+    p = min(max(p, 0), 100)
+    cFull = p // 8
+    p_str = '⬢' * cFull
+    p_str += '⬡' * (12 - cFull)
+    p_str = f"[{p_str}]"
+    return p_str
+
+def get_readable_message():
+    with download_dict_lock:
+        msg = ""
+        if STATUS_LIMIT is not None:
+            tasks = len(download_dict)
+            global pages
+            pages = ceil(tasks/STATUS_LIMIT)
+            if PAGE_NO > pages and pages != 0:
+                globals()['COUNT'] -= STATUS_LIMIT
+                globals()['PAGE_NO'] -= 1
+        msg += f"<b><i>VPS bot of <u>SL Torrenting Corp</u></i></b>\n\n"
+        for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
+            msg += f"<b>🌐 Name:</b> <code>{escape(str(download.name()))}</code>"
+            msg += f"\n<b>🌐 Status:</b> <i>{download.status()}</i>"
+            if download.status() not in [
+                MirrorStatus.STATUS_ARCHIVING,
+                MirrorStatus.STATUS_EXTRACTING,
+                MirrorStatus.STATUS_SPLITTING,
+                MirrorStatus.STATUS_SEEDING,
+            ]:
+                msg += f"\n{get_progress_bar_string(download)} {download.progress()}"
+                if download.status() == MirrorStatus.STATUS_CLONING:
+                    msg += f"\n<b>🌐 Cloned:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                elif download.status() == MirrorStatus.STATUS_UPLOADING:
+                    msg += f"\n<b>🌐 Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                else:
+                    msg += f"\n<b>🌐 Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                msg += f"\n<b>🌐 Speed:</b> {download.speed()} | <b>🌐 ETA:</b> {download.eta()}"
+                try:
+                    msg += f"\n<b>🔥Seeders:</b> {download.aria_download().num_seeders}" \
+                           f" | <b>🔥Peers:</b> {download.aria_download().connections}"
+                except:
+                    pass
+                try:
+                    msg += f"\n<b>🔥Seeders:</b> {download.torrent_info().num_seeds}" \
+                           f" | <b>🔥Leechers:</b> {download.torrent_info().num_leechs}"
+                except:
+                    pass
+                msg += f"\n<b>⏱ Elapsed : </b>{get_readable_time(time() - download.message.date.timestamp())}"
+                msg += f'\n<b>👤 User :<i> <a href="https://t.me/c/{str(download.message.chat.id)[4:]}/{download.message.message_id}">{download.message.from_user.first_name}</a></i></b>'
+                msg += f"\n<b>To Cancel❌:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+            elif download.status() == MirrorStatus.STATUS_SEEDING:
+                msg += f"\n<b>🌐 Size: </b>{download.size()}"
+                msg += f"\n<b>🌐 Speed: </b>{get_readable_file_size(download.torrent_info().upspeed)}/s"
+                msg += f" | <b>🌐 Uploaded: </b>{get_readable_file_size(download.torrent_info().uploaded)}"
+                msg += f"\n<b>🌐 Ratio: </b>{round(download.torrent_info().ratio, 3)}"
+                msg += f" | <b>🌐 Time: </b>{get_readable_time(download.torrent_info().seeding_time)}"
+                msg += f"\n<b>⏱ Elapsed : </b>{get_readable_time(time() - download.message.date.timestamp())}"
+                msg += f'\n<b>👤 User :<i> <a href="https://t.me/c/{str(download.message.chat.id)[4:]}/{download.message.message_id}">{download.message.from_user.first_name}</a></i></b>'
+                msg += f"\n<b>To Cancel❌:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+            else:
+                msg += f"\n<b>🌐 Size: </b>{download.size()}"
+            msg += "\n\n\n"
+            if STATUS_LIMIT is not None and index == STATUS_LIMIT:
+                break
+        bmsg = f"<b>CPU:</b> <code>{cpu_percent()}%</code> | <b>FREE:</b> <code>{get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}</code>"
+        bmsg += f"\n<b>RAM:</b> <code>{virtual_memory().percent}%</code> | <b>UPTIME:</b> <code>{get_readable_time(time() - botStartTime)}</code>"
+        dlspeed_bytes = 0
+        upspeed_bytes = 0
+        for download in list(download_dict.values()):
+            spd = download.speed()
+            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                if 'K' in spd:
+                    dlspeed_bytes += float(spd.split('K')[0]) * 1024
+                elif 'M' in spd:
+                    dlspeed_bytes += float(spd.split('M')[0]) * 1048576
+            elif download.status() == MirrorStatus.STATUS_UPLOADING:
+                if 'KB/s' in spd:
+                    upspeed_bytes += float(spd.split('K')[0]) * 1024
+                elif 'MB/s' in spd:
+                    upspeed_bytes += float(spd.split('M')[0]) * 1048576
+        bmsg += f"\n<b>DL:</b> <code>{get_readable_file_size(dlspeed_bytes)}/s</code> | <b>UL:</b> <code>{get_readable_file_size(upspeed_bytes)}/s</code>"
+        if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
+            msg += f"<b>📑 Page:</b> {PAGE_NO}/{pages} | <b>📋 Tasks:</b> {tasks}\n"
+            buttons = ButtonMaker()
+            buttons.sbutton("<<Previous", "status pre")
+            buttons.sbutton("Next>>", "status nex")
+            button = buttons.build_menu(2)
+            return msg + bmsg, button
+        return msg + bmsg, ""
+
+def turn(data):
+    try:
+        with download_dict_lock:
+            global COUNT, PAGE_NO
+            if data[1] == "nex":
+                if PAGE_NO == pages:
+                    COUNT = 0
+                    PAGE_NO = 1
+                else:
+                    COUNT += STATUS_LIMIT
+                    PAGE_NO += 1
+            elif data[1] == "pre":
+                if PAGE_NO == 1:
+                    COUNT = STATUS_LIMIT * (pages - 1)
+                    PAGE_NO = pages
+                else:
+                    COUNT -= STATUS_LIMIT
+                    PAGE_NO -= 1
+        return True
+    except:
+        return False
+
+def get_readable_time(seconds: int) -> str:
+    result = ''
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result
+
+def is_url(url: str):
+    url = re_findall(URL_REGEX, url)
+    return bool(url)
+
+def is_gdrive_link(url: str):
+    return "drive.google.com" in url
+
+def is_mega_link(url: str):
+    return "mega.nz" in url or "mega.co.nz" in url
+
+def is_gdtot_link(url: str):
+    url = re_match(r'https?://.+\.gdtot\.\S+', url)
+    return bool(url)
+
+def get_mega_link_type(url: str):
+    if "folder" in url:
+        return "folder"
+    elif "file" in url:
+        return "file"
+    elif "/#F!" in url:
+        return "folder"
+    return "file"
+
+def is_magnet(url: str):
+    magnet = re_findall(MAGNET_REGEX, url)
+    return bool(magnet)
+
+def new_thread(fn):
+    """To use as decorator to make a function call threaded.
+    Needs import
+    from threading import Thread"""
+
+    def wrapper(*args, **kwargs):
+        thread = Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+
+    return wrapper
+
+def get_content_type(link: str) -> str:
+    try:
+        res = rhead(link, allow_redirects=True, timeout=5, headers = {'user-agent': 'Wget/1.12'})
+        content_type = res.headers.get('content-type')
+    except:
+        try:
+            res = urlopen(link, timeout=5)
+            info = res.info()
+            content_type = info.get_content_type()
+        except:
+            content_type = None
+    return content_type
---
+++ bot/modules/clone.py
@@ -0,0 +1,144 @@
+from random import SystemRandom
+from string import ascii_letters, digits
+from telegram.ext import CommandHandler
+from threading import Thread
+from time import sleep
+from telegram import InlineKeyboardMarkup
+
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
+from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage, delete_all_messages, update_all_messages, sendStatusMessage, sendFile, sendMarkup
+from bot.helper.telegram_helper.filters import CustomFilters
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
+from bot import dispatcher, LOGGER, STOP_DUPLICATE, download_dict, download_dict_lock, Interval, CLONE_LIMIT
+from bot.helper.ext_utils.bot_utils import is_gdrive_link, new_thread, is_gdtot_link, get_readable_file_size
+from bot.helper.telegram_helper.button_build import ButtonMaker
+from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot
+from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
+
+def _clone(message, bot):
+    try:
+        buttons = ButtonMaker()
+        TITLE_NAME = "Join Channel"
+        CHANNEL_USERNAME = "SLTCUpdates"
+        uname = message.from_user.mention_html(message.from_user.first_name)
+        user = bot.get_chat_member(-1001691739650, message.from_user.id)
+        if user.status not in ['member', 'creator', 'administrator']:
+            buttons.buildbutton(f"{TITLE_NAME}", f"https://t.me/{CHANNEL_USERNAME}")
+            reply_markup = buttons.build_menu(1)
+            return sendMarkup(f"<b>Hey <i><u>{uname}️</u></i>,\n\nFirst join our updates channel</b>", bot, message, reply_markup)
+    except Exception as e:
+        LOGGER.info(str(e))
+
+    args = message.text.split()
+    reply_to = message.reply_to_message
+    link = ''
+    multi=1
+    if len(args) > 1:
+        link = args[1].strip()
+        if link.strip().isdigit():
+            multi = int(link)
+            link = ''
+        elif message.from_user.username:
+            tag = f"@{message.from_user.username}"
+        else:
+            tag = message.from_user.mention_html(message.from_user.first_name)
+        try:
+            tag1 = message.from_user.mention_html(message.from_user.first_name)
+            text1 = message.text
+            s1 = text1.split(' ', maxsplit=1)
+            s2 = s1[0].split('/', maxsplit=1)
+            msg = f"<b>User <i>{tag1}</i> sent:</b>\n<code>{link}</code>\n"
+            msg += f"<b>With Command:</b>\n<i>{s2[1]}</i>"
+            sendMessage(msg, bot, message)
+        except:
+            pass
+
+    if reply_to:
+        if len(link) == 0:
+            link = reply_to.text.split(maxsplit=1)[0].strip()
+        if reply_to.from_user.username:
+            tag = f"@{reply_to.from_user.username}"
+        else:
+            tag = reply_to.from_user.mention_html(reply_to.from_user.first_name)
+        try:
+            tag1 = reply_to.from_user.mention_html(reply_to.from_user.first_name)
+            text2 = message.text
+            s3 = text2.split('/', maxsplit=1)
+            msg = f"<b>User <i>{tag1}</i> sent:</b>\n<code>{link}</code>\n"
+            msg += f"<b>With Command:</b>\n<i>{s3[1]}</i>"
+            sendMessage(msg, bot, message)
+        except:
+            pass
+    is_gdtot = is_gdtot_link(link)
+    if is_gdtot:
+        try:
+            msg = sendMessage(f"Processing: <code>{link}</code>", bot, message)
+            link = gdtot(link)
+            deleteMessage(bot, msg)
+        except DirectDownloadLinkException as e:
+            deleteMessage(bot, msg)
+            return sendMessage(str(e), bot, message)
+    if is_gdrive_link(link):
+        gd = GoogleDriveHelper()
+        res, size, name, files = gd.helper(link)
+        if res != "":
+            return sendMessage(res, bot, message)
+        if STOP_DUPLICATE:
+            LOGGER.info('Checking File/Folder if already in Drive...')
+            cap, f_name = gd.drive_list(name, True, True)
+            if cap:
+                cap = f"File/Folder is already available in Drive. Here are the search results:\n\n{cap}"
+                sendFile(bot, message, f_name, cap)
+                return
+        if CLONE_LIMIT is not None:
+            LOGGER.info('Checking File/Folder Size...')
+            if size > CLONE_LIMIT * 1024 ** 3:
+                msg2 = f'Failed, Clone limit is {CLONE_LIMIT}GB.\nYour File/Folder size is {get_readable_file_size(size)}.'
+                return sendMessage(msg2, bot, message)
+        if multi > 1:
+            sleep(4)
+            nextmsg = type('nextmsg', (object, ), {'chat_id': message.chat_id, 'message_id': message.reply_to_message.message_id + 1})
+            nextmsg = sendMessage(message.text.replace(str(multi), str(multi - 1), 1), bot, nextmsg)
+            nextmsg.from_user.id = message.from_user.id
+            sleep(4)
+            Thread(target=_clone, args=(nextmsg, bot)).start()
+        if files <= 20:
+            msg = sendMessage(f"Cloning: <code>{link}</code>", bot, message)
+            result, button = gd.clone(link)
+            deleteMessage(bot, msg)
+        else:
+            drive = GoogleDriveHelper(name)
+            gid = ''.join(SystemRandom().choices(ascii_letters + digits, k=12))
+            clone_status = CloneStatus(drive, size, message, gid)
+            with download_dict_lock:
+                download_dict[message.message_id] = clone_status
+            sendStatusMessage(message, bot)
+            result, button = drive.clone(link)
+            with download_dict_lock:
+                del download_dict[message.message_id]
+                count = len(download_dict)
+            try:
+                if count == 0:
+                    Interval[0].cancel()
+                    del Interval[0]
+                    delete_all_messages()
+                else:
+                    update_all_messages()
+            except IndexError:
+                pass
+        cc = f'\n\n<b>➩👤 cc: </b>{tag}'
+        if button in ["cancelled", ""]:
+            sendMessage(f"{tag} {result}", bot, message)
+        else:
+            sendMarkup(result + cc, bot, message, button)
+            LOGGER.info(f'Cloning Done: {name}')
+    else:
+        sendMessage("Send Gdrive link along with command or by replying to the link by command\n\n<b>Multi links only by replying to first link/file:</b>\n<code>/cmd</code> 10(number of links/files)", bot, message)
+
+@new_thread
+def cloneNode(update, context):
+    _clone(update.message, context.bot)
+
+clone_handler = CommandHandler(BotCommands.CloneCommand, cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+dispatcher.add_handler(clone_handler)
